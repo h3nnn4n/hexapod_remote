99a319c363972d4c41b5faac348c122d41e1de47
h3nnn4n <git@renan.digital>
2023-06-26 23:49:09 -0300

    Support sending commands in bulk

--- gait_demo.py
+++ gait_demo.py
@@ -13,12 +13,25 @@ def main():
 
     sleep(0.1)
 
-    serial.send_command("DISABLE_SERVOS")
-
-    for i in range(6):
-        serial.send_command(f"SET_LEG_ANGLES {i} 0 90 175")
-
     serial.send_command("ENABLE_SERVOS")
+
+    serial.disable_auto_send()
+
+    while True:
+        serial.set_leg_position(4, 0, 140, -120)
+        serial.set_leg_position(1, 0, 140, -120)
+        serial.send_commands()
+        # sleep(0.5)
+
+        serial.set_leg_position(4, -50, 140, -140)
+        serial.set_leg_position(1, -50, 140, -140)
+        serial.send_commands()
+        # sleep(0.5)
+
+        serial.set_leg_position(4, 50, 140, -140)
+        serial.set_leg_position(1, 50, 140, -140)
+        serial.send_commands()
+        # sleep(0.5)
 
 
 if __name__ == "__main__":
--- hexapod_remote/serial.py
+++ hexapod_remote/serial.py
@@ -6,10 +6,47 @@ from hexapod_remote import config
 
 
 class Serial:
-    def __init__(self):
+    def __init__(self, auto_send: bool = True):
         self._port = serial.Serial(config.SERIAL_PORT, config.BAUD_RATE, timeout=config.SERIAL_TIMEOUT)
+        self._auto_send = auto_send
+        self._queue = []
+
+    def disable_auto_send(self) -> None:
+        self._auto_send = False
+
+    def enable_auto_send(self) -> None:
+        self._auto_send = True
+
+    def set_auto_send(self, auto_send: bool) -> None:
+        self._auto_send = auto_send
+
+        if auto_send:
+            self.send_commands()
+
+    def send_commands(self) -> None:
+        commands = "\n".join(self._queue)
+        self._queue.clear()
+        self._send_command(commands)
+
+    def set_leg_angles(self, leg_index: int, coxa: float, femur: float, tibia: float) -> None:
+        if leg_index not in range(6):
+            raise ValueError("leg_index must be in range(6)")
+
+        self.send_command(f"SET_LEG_ANGLES {leg_index} {coxa} {femur} {tibia}")
+
+    def set_leg_position(self, leg_index: int, x: float, y: float, z: float) -> None:
+        if leg_index not in range(6):
+            raise ValueError("leg_index must be in range(6)")
+
+        self.send_command(f"SET_LEG_POSITION {leg_index} {x} {y} {z}")
 
     def send_command(self, command: str) -> None:
+        if not self._auto_send:
+            return self._queue.append(command)
+
+        self._send_command(command)
+
+    def _send_command(self, command: str) -> None:
         print(command)
         self.write(command)
         self.wait_for_ok()
