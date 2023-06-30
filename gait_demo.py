a288de681a2b63c17836e1ad850c2723545bd5ed
h3nnn4n <git@renan.digital>
2023-06-30 16:11:34 -0300

    Set safer startup sequence
    one one pulled too many amps, killed the voltage regulator causing it to
    fail shorted and send 24v into the bot destroying a lot of things D:

--- gait_demo.py
+++ gait_demo.py
@@ -3,7 +3,7 @@ from time import sleep
 from hexapod_remote.serial import Serial
 
 
-def main():
+def connect() -> Serial:
     print("starting...")
 
     serial = Serial()
@@ -11,47 +11,44 @@ def main():
 
     print("ready")
 
-    sleep(0.1)
-
     serial.send_command("ENABLE_SERVOS")
 
-    serial.disable_auto_send()
+    return serial
+
+
+def set_starting_position(serial: Serial) -> None:
+    """
+    Set all legs to the "rest" position, one by one.
+
+    To be safer, the robot should already be physically close to the target
+    positions.
+    """
+    sleep(0.5)
     for i in range(6):
-        serial.set_leg_position(i, 0, 100, -100)
-    serial.send_commands()
+        sleep(0.5)
+        serial.set_leg_angles(i, 0, 90, 175)
 
-    # serial.enable_auto_send()
 
-    # serial.set_leg_position(2, 50, 100, -140)
+def standup(serial: Serial, height: float = 100, n_steps: int = 15) -> None:
+    serial.disable_auto_send()
 
-    # serial.send_command("READ_LEG_INFO 2")
+    starting_height = 80
+    step = (height - starting_height) / n_steps
 
-    return
+    for i in range(n_steps + 1):
+        h = 0.0 - (starting_height + i * step)
+        h = round(h, 1)
 
-    legs = [5, 3, 1]
-    # legs = [0, 3, 2, 5]
-    # legs = [0, 3]
+        for leg_index in range(6):
+            serial.set_leg_position(leg_index, 0, 70, h)
 
-    while True:
-        for leg in legs:
-            serial.set_leg_position(leg, -30, 100, -120)
         serial.send_commands()
-        sleep(0.25)
 
-        for leg in legs:
-            serial.set_leg_position(leg, 0, 100, -120)
-        serial.send_commands()
-        sleep(0.25)
 
-        for leg in legs:
-            serial.set_leg_position(leg, 30, 100, -120)
-        serial.send_commands()
-        sleep(0.25)
-
-        for leg in legs:
-            serial.set_leg_position(leg, 0, 100, -120)
-        serial.send_commands()
-        sleep(0.25)
+def main():
+    serial = connect()
+    set_starting_position(serial)
+    standup(serial, 120)
 
 
 if __name__ == "__main__":
