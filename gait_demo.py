4d1523ff4576d5c592df252eea91419485c3a4d3
h3nnn4n <git@renan.digital>
2023-07-01 10:40:19 -0300

    Set multiple demos

--- gait_demo.py
+++ gait_demo.py
@@ -49,7 +49,7 @@ def set_height(serial: Serial, height: float = 100) -> None:
     serial.disable_auto_send()
 
     for leg_index in range(6):
-        serial.set_leg_position(leg_index, 0, 60, -height)
+        serial.set_leg_position(leg_index, 0, 95, -height)
 
     serial.send_commands()
 
@@ -67,28 +67,19 @@ def calibration_position(serial: Serial) -> None:
     serial.send_commands()
 
 
-def main():
-    serial = connect()
-
-    sleep(2.5)
-    serial.set_leg_mode("INSTANTANEOUS")
-    set_height(serial, 140)
-    sleep(2.5)
-
-    serial.enable_auto_send()
-
-    serial.set_legs_speed(2500.0)
-    serial.set_leg_mode("CONSTANT_SPEED")
-
-    serial.disable_auto_send()
-
+def walk_forward(serial: Serial) -> None:
     sleep_time = 1.5
     leg_group_1 = [0, 2, 4]
     leg_group_2 = [1, 3, 5]
-    ground_height = -140
-    feet_up_height = -100
-    distance_from_body = 60
+    ground_height = -100
+    feet_up_height = -85
+    distance_from_body = 95
     stride_length = 35
+
+    serial.disable_auto_send()
+    serial.set_legs_speed(2500.0)
+    serial.set_leg_mode("CONSTANT_SPEED")
+    serial.send_commands()
 
     while True:
         # Part 1
@@ -122,6 +113,87 @@ def main():
         serial.send_commands()
         sleep(sleep_time)
         print()
+
+
+def translate_body_up_and_down(serial: Serial) -> None:
+    sleep_time = 3.0
+    low_height = -175
+    high_height = -100
+    distance_from_body = 95
+
+    serial.disable_auto_send()
+    serial.set_legs_speed(2500.0)
+    serial.set_leg_mode("CONSTANT_SPEED")
+    serial.send_commands()
+
+    while True:
+        for leg in range(6):
+            serial.set_leg_position(leg, 0, distance_from_body, low_height)
+        serial.send_commands()
+        sleep(sleep_time)
+        print()
+
+        for leg in range(6):
+            serial.set_leg_position(leg, 0, distance_from_body, high_height)
+        serial.send_commands()
+        sleep(sleep_time)
+        print()
+
+
+def walk_in_place(serial: Serial) -> None:
+    sleep_time = 0.75
+    leg_group_1 = [0, 2, 4]
+    leg_group_2 = [1, 3, 5]
+    ground_height = -100
+    feet_up_height = -75
+    distance_from_body = 95
+    stride_length = 0
+
+    serial.disable_auto_send()
+    serial.set_legs_speed(7500.0)
+    serial.set_leg_mode("CONSTANT_SPEED")
+    serial.send_commands()
+
+    while True:
+        # Part 1
+        for leg in leg_group_1:
+            serial.set_leg_position(leg, -stride_length, distance_from_body, feet_up_height)
+        serial.send_commands()
+        sleep(sleep_time)
+        print()
+
+        for leg in leg_group_1:
+            serial.set_leg_position(leg, -stride_length, distance_from_body, ground_height)
+        serial.send_commands()
+        sleep(sleep_time)
+        print()
+
+        # Part 2
+        for leg in leg_group_2:
+            serial.set_leg_position(leg, stride_length, distance_from_body, feet_up_height)
+        serial.send_commands()
+        sleep(sleep_time)
+        print()
+
+        for leg in leg_group_2:
+            serial.set_leg_position(leg, -stride_length, distance_from_body, ground_height)
+        serial.send_commands()
+        sleep(sleep_time)
+        print()
+
+
+def main():
+    serial = connect()
+
+    sleep(2.5)
+    serial.set_leg_mode("INSTANTANEOUS")
+    set_starting_position(serial)
+    set_height(serial, 110)
+    sleep(2.5)
+
+    # walk_in_place(serial)
+    # walk_forward(serial)
+    translate_body_up_and_down(serial)
 
 
 if __name__ == "__main__":
