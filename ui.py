ff6b52c2a072309b6258cd94520c367fdb795b5b
h3nnn4n <git@renan.digital>
2023-08-09 00:26:51 -0300

    Set button to close ui

--- ui.py
+++ ui.py
@@ -4,15 +4,33 @@ from PyQt5 import QtWidgets, uic
 
 
 class Ui(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(Ui, self).__init__()
-        uic.loadUi("ui/main_window.ui", self)
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+        self.setup_ui()
         self.show()
+
+    def setup_ui(self):
+        uic.loadUi("ui/main_window.ui", self)
+
+        button = self.findChild(QtWidgets.QPushButton, "quitPushButton")
+        button.clicked.connect(self.close_ui)  # type:ignore
+
+        # button = self.findChild(QtWidgets.QAction, "actionExit")
+        # button.clicked.connect(self.close_ui)  # type:ignore
+
+        # button = self.findChild(QtWidgets.QAction, "actionQuit")
+        # button.clicked.connect(self.close_ui)  # type:ignore
+
+    def close_ui(self):
+        self.app.exit(0)
 
 
 def main() -> None:
     app = QtWidgets.QApplication(sys.argv)
-    Ui()
+    window = Ui(app=app)
+    window.show()
     app.exec_()
 
 
